de2fdc75ec29fff84295485a5a23bcd27c0b7bbc
Akshata Jadhav <akshatajadhav@Akshatas-MacBook-Pro.local>
2021-10-09 01:39:30 -0400

    first ui draft

---
+++ AED/src/model/Car.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author akshatajadhav
+ */
+public class Car {
+
+    private Long serialNumber;
+    private String modelNumber;
+    private String make;
+    private int manufacturedYear;
+    private String type;
+    private int seats;
+    private boolean availability;
+    private String city;
+    private Date maintenanceCertificateDate;
+
+    public Long getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(Long serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getModelNumber() {
+        return modelNumber;
+    }
+
+    public void setModelNumber(String modelNumber) {
+        this.modelNumber = modelNumber;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public int getManufacturedYear() {
+        return manufacturedYear;
+    }
+
+    public void setManufacturedYear(int manufacturedYear) {
+        this.manufacturedYear = manufacturedYear;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    public boolean isAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(boolean availability) {
+        this.availability = availability;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Date getMaintenanceCertificateDate() {
+        return maintenanceCertificateDate;
+    }
+
+    public void setMaintenanceCertificateDate(Date maintenanceCertificateDate) {
+        this.maintenanceCertificateDate = maintenanceCertificateDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" + "serialNumber=" + serialNumber + ", modelNumber=" + modelNumber + ", make=" + make + ", manufacturedYear=" + manufacturedYear + ", type=" + type + ", seats=" + seats + ", availability=" + availability + ", city=" + city + ", maintenanceCertificateDate=" + maintenanceCertificateDate + '}';
+    }
+
+    public Object toList() {
+        Object ls[] = new Object[9];
+        ls[0] = String.valueOf(serialNumber);
+        ls[1] = modelNumber;
+        ls[2] = make;
+        ls[3] = String.valueOf(manufacturedYear);
+        ls[4] = type;
+        ls[5] = String.valueOf(seats);
+        ls[6] = String.valueOf(availability);
+        ls[7] = city;
+        if (maintenanceCertificateDate != null) {
+            ls[8] = (new SimpleDateFormat("MM/dd/yyyy")).format(maintenanceCertificateDate);
+        } else {
+            ls[8] = null;
+        }
+        return ls;
+    }
+}
---
+++ AED/src/model/Uber.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author akshatajadhav
+ */
+public class Uber {
+    private List<Car> cars;
+    private List<Long> serialNumbers;
+    private Date lastModified;
+
+    public Uber() {
+        cars = new ArrayList<Car>();
+        serialNumbers = new ArrayList<Long>();
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+        this.lastModified = new Date();
+    }
+    
+    public void addCarToFleet(Car car) {
+        this.cars.add(car);
+        this.lastModified = new Date();
+        this.serialNumbers.add(car.getSerialNumber());
+    }
+
+    public List<Long> getSerialNumbers() {
+        return serialNumbers;
+    }
+
+    public void setSerialNumbers(List<Long> serialNumbers) {
+        this.serialNumbers = serialNumbers;
+    }
+    
+    public void addSerialNumbers(Long serialNumbers) {
+        this.serialNumbers.add(serialNumbers);
+    }
+    
+    public void removeSerialNumbers(Long serialNumbers) {
+        this.serialNumbers.remove(serialNumbers);
+    }
+    
+    public Date getLastModified() {
+        return lastModified;
+    }    
+
+    @Override
+    public String toString() {
+        return "Uber{" + "cars=" + cars + ", serialNumbers=" + serialNumbers + ", lastModified=" + lastModified + '}';
+    }
+    
+}
